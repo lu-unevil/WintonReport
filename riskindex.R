00039f5ad52769c62106dbc2c2c7112c79c43149
Dmytro Perepolkin <dperepolkin@gmail.com>
2020-05-19 17:44:58 +0200

    fix reversal of CanadaQ_3

--- riskindex.R
+++ riskindex.R
@@ -18,7 +18,8 @@ rescaled_df <- df %>%
   filter(!is.na(code)) %>% 
   left_join(max_lbls, by="var") %>% 
   # The rescaling from 1-5 to 1-7 is done using 1.5*(old value) - 0.5
-  mutate(scaled_code=ifelse(max_code==5, 1.5*code-0.5, code)) %>% 
+  mutate(rev_code=ifelse(var=="CanadaQ_3", 6-code, code),
+         scaled_code=ifelse(max_code==5, 1.5*rev_code-0.5, rev_code)) %>% 
   group_by(rowid, Residency) %>% 
   summarise(mean_code = mean(scaled_code))
 
